1edc5e29d6ebf4ad31e1ea9afc4bf55e8ecd45e6
Yuri Cavalcante <yuri.scavalcante@outlook.com>
2023-12-19 15:45:58 -0300

    first commit

---
+++ index.rb
@@ -0,0 +1,13 @@
+print "Digite seu nome: "
+
+nome = gets.chomp
+
+print "Digite seu sobrenome: "
+
+sobrenome = gets.chomp
+
+print "Digite sua idade: "
+
+idade = gets.chomp
+
+print "nome completo: #{nome} #{sobrenome}, idade: #{idade}"
